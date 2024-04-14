8b3e0be45fb54315560d6f988b996f813d5a555c
matzalmeida <37148767+matzalmeida@users.noreply.github.com>
2024-04-14 14:44:42 -0300

    Função adicionada
    Adicionado indicador de número de escolhas da questão.

--- script.js
+++ script.js
@@ -1,44 +1,57 @@
-
-question_id = 0;
-limit = 1;
-
+const limit = 1;
 const quiz_question = document.getElementById("question");
 const quiz_answers_ul = document.getElementById("answers");
 const quiz_category = document.getElementById("category");
 const quiz_difficulty = document.getElementById("difficulty");
-let quiz_multiple = "false";
+const quiz_type = document.getElementById("type");
+const quiz_check_answer = document.getElementById("check-answer");
+
+let quiz_multiple = "false", data = "";
+
+function eventListners(){
+    quiz_check_answer.addEventListener('click', checkAnswer);
+}
 
 document.addEventListener('DOMContentLoaded', () =>{
     loadQuestion();
+    eventListners();
 });
 
 async function loadQuestion() {
     let APIUrl = `https://quizapi.io/api/v1/questions?apiKey=yj02XyYZMCXObC1EPX6MRzu1yF5Vf4NVc7rQ6Nzz&limit=${limit}`;
-    const result = await fetch(`${APIUrl}`);
-    const data = await result.json();
+    let result = await fetch(`${APIUrl}`);
+    data = await result.json();
     console.log(data);
-    showQuestion(data[question_id]);
+    showQuestion(data[0]);
 }
 
 function showQuestion(data){
+    let quiz_correct_answers = Object.values(data.correct_answers);
+    let question_tags = Object.values(data.tags);
+    console.log(question_tags);
     let quiz_answers = data.answers;
     quiz_question.innerHTML = '';
     let title = document.createTextNode(data.question);
     quiz_question.appendChild(title);
-    quiz_category.innerHTML = data.category === "" ? "None" : data.category;
+    
+    quiz_category.innerHTML = data.category === "" ? "Uncategorized" : data.category;
+    /* quiz difficulty */
     quiz_difficulty.innerHTML = data.difficulty;
     quiz_difficulty.classList.remove("easy");
     quiz_difficulty.classList.remove("medium");
     quiz_difficulty.classList.remove("hard");
     quiz_difficulty.classList.add(data.difficulty);
+    /* quiz type */
     quiz_multiple = data.multiple_correct_answers;
+    quiz_type.innerHTML = quiz_multiple === "true" ? "Multiple Choice" : "Single Choice";
+    /* creation of answer list elements */
     for (answer in quiz_answers) {
-        li = document.createElement('li');
-        li.classList.add("answer")
-        li.classList.add(answer)
         if (quiz_answers[answer] == null){
             break;
         }
+        li = document.createElement('li');
+        li.classList.add("answer")
+        li.classList.add(answer)
         let text = document.createTextNode(quiz_answers[answer]);
         li.appendChild(text);
         quiz_answers_ul.appendChild(li);
@@ -49,12 +62,14 @@ function showQuestion(data){
 function selectOption(){
     quiz_answers_ul.querySelectorAll('li').forEach((answer) => {
         answer.addEventListener('click', () => {
+            /* resposta única */
             if(quiz_multiple === "false"){
                 if(quiz_answers_ul.querySelector('.selected')){
                     const activeOption = quiz_answers_ul.querySelector('.selected');
                     activeOption.classList.remove('selected');
                 }
                 answer.classList.add('selected');
+            /* múltipla escolha */
             } else {
                 if (answer.classList.contains('selected')){
                     answer.classList.remove('selected');
@@ -64,4 +79,9 @@ function selectOption(){
             }
         });
     });
+}
+
+function checkAnswer(){
+    console.log("checando");
+    quiz_check_answer.disabled = true;
 }
