db4cd84383aed517337c27a961a54ed419616cd1
matzalmeida <37148767+matzalmeida@users.noreply.github.com>
2024-04-15 01:37:24 -0300

    Novas funcionalidades
    Funcionalidade para checar as respostas adicionada. Feedback intuitivo através de cores.
    
    Funcionalidade de ir para a próxima questão adicionada.
    
    TODO: Tela de configurações. Poderá ser selecionado a dificuldade das questões, além das categorias e tags possíveis.

--- script.js
+++ script.js
@@ -1,75 +1,147 @@
 const limit = 1;
+
+/* load UI elements */
+const quiz_container = document.getElementById("quiz-container");
 const quiz_question = document.getElementById("question");
 const quiz_answers_ul = document.getElementById("answers");
 const quiz_category = document.getElementById("category");
 const quiz_difficulty = document.getElementById("difficulty");
 const quiz_type = document.getElementById("type");
 const quiz_check_answer = document.getElementById("check-answer");
+const quiz_result = document.getElementById("result");
+const quiz_current_score = document.getElementById("current-score");
+const quiz_total_questions = document.getElementById("total-questions");
 
-let quiz_multiple = "false", data = "";
+/* store some question data */
+let quiz_multiple = false, data = "";
+let quiz_number_answers = 0;
+let quiz_correct_answers;
+let current_score = 0;
+let total_questions = 0;
 
+let quiz_ended = false;
+
+/* creates eventlistners for buttons */
 function eventListners(){
     quiz_check_answer.addEventListener('click', checkAnswer);
 }
 
+/* check if DOM content is loaded */
 document.addEventListener('DOMContentLoaded', () =>{
     loadQuestion();
     eventListners();
 });
 
+/* load question from API */
 async function loadQuestion() {
-    let APIUrl = `https://quizapi.io/api/v1/questions?apiKey=yj02XyYZMCXObC1EPX6MRzu1yF5Vf4NVc7rQ6Nzz&limit=${limit}`;
-    let result = await fetch(`${APIUrl}`);
-    data = await result.json();
-    console.log(data);
+    do{  
+        let APIUrl = `https://quizapi.io/api/v1/questions?apiKey=yj02XyYZMCXObC1EPX6MRzu1yF5Vf4NVc7rQ6Nzz&limit=${limit}`;
+        let result;
+        do{
+            result = await fetch(`${APIUrl}`);
+        } while(!result.ok);
+        data = await result.json();
+        console.log("question data");
+        console.log(data);
+    } while(isValidQuestion() === false);
+    quiz_multiple = isMultipleChoice();
     showQuestion(data[0]);
 }
 
+/* check if api question is valid. some of the questions don't seem to have answers. */
+function isValidQuestion() {
+    quiz_correct_answers = Object.values(data[0].correct_answers);
+    for (answer of quiz_correct_answers)
+        if(answer === 'true') return true;
+    return false;
+}
+
+/* check if question is a multiple choice type. some of the questions seem to be wrongly classified by the api. */
+function isMultipleChoice(){
+    let count = 0;
+    quiz_correct_answers = Object.values(data[0].correct_answers);
+    for (answer of quiz_correct_answers){
+        if (answer === 'true') count += 1;
+        if (count > 1) return true;
+    }
+    return false;
+}
+
+/* show question data using UI */
 function showQuestion(data){
-    let quiz_correct_answers = Object.values(data.correct_answers);
-    let question_tags = Object.values(data.tags);
-    console.log(question_tags);
-    let quiz_answers = data.answers;
+    quiz_correct_answers = Object.values(data.correct_answers);
+    /* quiz score */
+    updateScore();
+
+    /* question title */
     quiz_question.innerHTML = '';
     let title = document.createTextNode(data.question);
     quiz_question.appendChild(title);
-    
+
+    /* question category */
     quiz_category.innerHTML = data.category === "" ? "Uncategorized" : data.category;
-    /* quiz difficulty */
+
+    /* question difficulty */
     quiz_difficulty.innerHTML = data.difficulty;
-    quiz_difficulty.classList.remove("easy");
-    quiz_difficulty.classList.remove("medium");
-    quiz_difficulty.classList.remove("hard");
+    quiz_difficulty.classList.remove("Easy");
+    quiz_difficulty.classList.remove("Medium");
+    quiz_difficulty.classList.remove("Hard");
     quiz_difficulty.classList.add(data.difficulty);
-    /* quiz type */
-    quiz_multiple = data.multiple_correct_answers;
-    quiz_type.innerHTML = quiz_multiple === "true" ? "Multiple Choice" : "Single Choice";
+
+    /* question tags */
+    let quiz_taglist = document.getElementById("taglist");
+    let question_tags = Object.values(data.tags);
+    console.log("question tags");
+    console.log(question_tags);
+    quiz_taglist.innerHTML = '';
+    for (tag of question_tags){
+        /* avoid repeating category on tags */
+        if (tag.name === data.category)
+            continue;
+        let span = document.createElement('span');
+        span.classList.add('tag');
+        span.innerHTML = tag.name;
+        quiz_taglist.appendChild(span);
+    }
+
+    /* question type */
+    quiz_type.innerHTML = quiz_multiple ? "Multiple Choice" : "Single Choice";
+
     /* creation of answer list elements */
+    let quiz_answers = data.answers;
+    quiz_answers_ul.innerHTML = '';
+    quiz_number_answers = 0;
     for (answer in quiz_answers) {
         if (quiz_answers[answer] == null){
             break;
         }
+        quiz_number_answers++;
         li = document.createElement('li');
         li.classList.add("answer")
-        li.classList.add(answer)
+        li.id = answer;
         let text = document.createTextNode(quiz_answers[answer]);
         li.appendChild(text);
         quiz_answers_ul.appendChild(li);
     }
+
+    /* creation of selectors for answer list elements */
     selectOption();
 }
 
+/* creates selectors for answer list elements */
 function selectOption(){
     quiz_answers_ul.querySelectorAll('li').forEach((answer) => {
         answer.addEventListener('click', () => {
-            /* resposta única */
-            if(quiz_multiple === "false"){
+            if (quiz_ended === true)
+                return;
+            /* single choice */
+            if(quiz_multiple === false){
                 if(quiz_answers_ul.querySelector('.selected')){
                     const activeOption = quiz_answers_ul.querySelector('.selected');
                     activeOption.classList.remove('selected');
                 }
                 answer.classList.add('selected');
-            /* múltipla escolha */
+            /* multiple choice */
             } else {
                 if (answer.classList.contains('selected')){
                     answer.classList.remove('selected');
@@ -81,7 +153,72 @@ function selectOption(){
     });
 }
 
+/* update gamescore */
+function updateScore(){
+    quiz_current_score.innerHTML = String(current_score);
+    quiz_total_questions.innerHTML = String(total_questions);
+}
+
+/* check if answer is right */
 function checkAnswer(){
-    console.log("checando");
-    quiz_check_answer.disabled = true;
+    if (quiz_ended){
+        quiz_ended = false;
+        quiz_result.innerHTML = '';
+        quiz_check_answer.innerHTML = "Check Answer";
+        loadQuestion();
+        return;
+    }
+
+    let correct = true;
+    let selected = false;
+    /* get all answer elements */
+    let answers = [
+        document.getElementById("answer_a"),
+        document.getElementById("answer_b"),
+        document.getElementById("answer_c"),
+        document.getElementById("answer_d"),
+        document.getElementById("answer_e"),
+        document.getElementById("answer_f"),
+    ];
+
+    /* check if at least one alternative is selected */
+    for (var i = 0; i < quiz_number_answers; i++)
+        if (answers[i].classList.contains("selected"))
+            selected = true;
+    if (selected === false){
+        quiz_result.innerHTML = `<p><i class="fas fa-exclamation"></i>You need to select an alternative.</p>`;
+        return;
+    }
+
+    /* loop through answers */
+    for (var i = 0; i < quiz_number_answers; i++){
+        /* tint correct and wrong selected answers */
+        let answer_selected = answers[i].classList.contains("selected");
+        if (quiz_correct_answers[i] === "true") {
+            answers[i].classList.add(quiz_multiple && !answer_selected ? "correct-multiple" : "correct");
+        } else if(answer_selected){
+            answers[i].classList.add("wrong");
+        } else {
+            answers[i].classList.add("unselected");
+        }
+
+        /* checks if selected answer is correct */
+        if ((quiz_correct_answers[i] === "true") === answers[i].classList.contains("selected")){
+            /* do nothing */
+        }
+        else {
+            correct = false;
+        }
+    }
+
+    /* need to select an alternative */
+    quiz_ended = true;
+    quiz_check_answer.innerHTML = "Next Question";
+    total_questions++;
+    if (correct) {
+        current_score++;
+        quiz_result.innerHTML = `<p><i class="fas fa-check"></i>Correct Answer!</p>`;
+    } else
+        quiz_result.innerHTML = `<p><i class="fas fa-times"></i>Incorrect Answer!</p>`;
+    updateScore();
 }
