6ab70016846e49837b2aa0a61f41019e3fcb8902
matzalmeida <37148767+matzalmeida@users.noreply.github.com>
2024-04-17 19:54:28 -0300

    Adicionadas novas telas e funcionalidades
    Protótipo inicial da página de configurações criado, adicionado botões de toggle entre páginas.
    
    TODO: Conectar dados de configuração no script.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const limit = 1;
+let apiKey = "yj02XyYZMCXObC1EPX6MRzu1yF5Vf4NVc7rQ6Nzz";
 
-/* load UI elements */
+/* load quiz UI elements */
 const quiz_container = document.getElementById("quiz-container");
 const quiz_question = document.getElementById("question");
 const quiz_answers_ul = document.getElementById("answers");
@@ -12,6 +12,14 @@ const quiz_result = document.getElementById("result");
 const quiz_current_score = document.getElementById("current-score");
 const quiz_total_questions = document.getElementById("total-questions");
 
+const settings_container = document.getElementById("settings-container");
+
+const stats_container= document.getElementById("statistics-container");
+
+const wrapper_footer_settings = document.getElementById("button-settings");
+const wrapper_footer_game = document.getElementById("button-game");
+const wrapper_footer_stats = document.getElementById("button-stats");
+
 /* store some question data */
 let quiz_multiple = false, data = "";
 let quiz_number_answers = 0;
@@ -21,13 +29,80 @@ let total_questions = 0;
 
 let quiz_ended = false;
 
+/* store settings data */
+let settings_tags, settings_categories;
+let tags_data, categories_data;
+
+/* load settings UI elements */
+const settings_tags_options = document.getElementById("settings-tags");
+const settings_categories_options = document.getElementById("settings-categories");
+
+async function loadTags() {
+    let APIUrl = `https://quizapi.io/api/v1/tags?apiKey=${apiKey}`;
+    let result;
+    do{
+        result = await fetch(`${APIUrl}`);
+    } while(!result.ok);
+    tags_data = await result.json();
+    let tags_list = [];
+    /* filter list for tags that return no questions. */
+    let filter_tags = [
+        "dev","Ruby","Undefined", "postgres", "AWS", "Css", "C", "Java", "Swift",
+        "Blockchain", "VueJS", ".Net"
+    ];
+    for (tag of tags_data){
+        if (!filter_tags.includes(tag.name))
+            tags_list.push(tag.name);
+    }
+    console.log("tags data");
+    console.log(tags_list);
+}
+
+async function loadCategories(){
+    let APIUrl = `https://quizapi.io/api/v1/categories?apiKey=${apiKey}`;
+    let result;
+    do{
+        result = await fetch(`${APIUrl}`);
+    } while(!result.ok);
+    categories_data = await result.json();
+    let categories_list = [];
+    for (category of categories_data){
+        categories_list.push(category.name);
+    }
+    console.log("categories data");
+    console.log(categories_list);
+}
+
 /* creates eventlistners for buttons */
 function eventListners(){
     quiz_check_answer.addEventListener('click', checkAnswer);
+    wrapper_footer_settings.addEventListener('click', gotoSettings);
+    wrapper_footer_game.addEventListener('click', gotoQuiz);
+    wrapper_footer_stats.addEventListener('click', gotoStats);
+}
+
+function gotoStats(){
+    settings_container.classList.add("hidden");
+    quiz_container.classList.add("hidden");
+    stats_container.classList.remove("hidden");
+}
+
+function gotoQuiz(){
+    settings_container.classList.add("hidden");
+    quiz_container.classList.remove("hidden");
+    stats_container.classList.add("hidden");
+}
+
+function gotoSettings(){
+    settings_container.classList.remove("hidden");
+    quiz_container.classList.add("hidden");
+    stats_container.classList.add("hidden");
 }
 
 /* check if DOM content is loaded */
 document.addEventListener('DOMContentLoaded', () =>{
+    loadTags();
+    loadCategories();
     loadQuestion();
     eventListners();
 });
@@ -35,9 +110,10 @@ document.addEventListener('DOMContentLoaded', () =>{
 /* load question from API */
 async function loadQuestion() {
     do{  
-        let APIUrl = `https://quizapi.io/api/v1/questions?apiKey=yj02XyYZMCXObC1EPX6MRzu1yF5Vf4NVc7rQ6Nzz&limit=${limit}`;
+        let APIUrl = `https://quizapi.io/api/v1/questions?apiKey=${apiKey}&limit=1`;
         let result;
         do{
+            console.log("fetching question API...");
             result = await fetch(`${APIUrl}`);
         } while(!result.ok);
         data = await result.json();
@@ -45,6 +121,9 @@ async function loadQuestion() {
         console.log(data);
     } while(isValidQuestion() === false);
     quiz_multiple = isMultipleChoice();
+    quiz_ended = false;
+    quiz_result.innerHTML = '';
+    quiz_check_answer.innerHTML = "Check Answer";
     showQuestion(data[0]);
 }
 
@@ -79,7 +158,7 @@ function showQuestion(data){
     quiz_question.appendChild(title);
 
     /* question category */
-    quiz_category.innerHTML = data.category === "" ? "Uncategorized" : data.category;
+    quiz_category.innerHTML = data.category === "" || data.category === "uncategorized" ? "Uncategorized" : data.category;
 
     /* question difficulty */
     quiz_difficulty.innerHTML = data.difficulty;
@@ -162,9 +241,6 @@ function updateScore(){
 /* check if answer is right */
 function checkAnswer(){
     if (quiz_ended){
-        quiz_ended = false;
-        quiz_result.innerHTML = '';
-        quiz_check_answer.innerHTML = "Check Answer";
         loadQuestion();
         return;
     }
